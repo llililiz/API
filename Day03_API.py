bb00359ddae668d25a0a712ad963fe67368ebf4d
KIM DONG HYEON <llililiz@gmail.com>
2026-03-09 13:36:54 +0900

    first commit

---
+++ Day03_API.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/employees")
+def get_employees(name):
+    return {f"{name}님, 환영합니다!"}
+
